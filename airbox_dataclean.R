8bcd8974ef5fc327306dd41587ea05d49079d904
Sherry <sherrylin42@gmail.com>
2018-06-20 19:48:05 +0800

    Spatial anomaly optimized with parallel computing. Done spatial and temporal anomaly detction. Airbox_processed ready for modelling

--- airbox_dataclean.R
+++ airbox_dataclean.R
@@ -5,6 +5,11 @@ library(rgdal)
 library(maptools)
 library(maps)
 library(ggmap)
+library(doParallel)
+library(foreach)
+
+cl <- makePSOCKcluster(detectCores() - 1)
+registerDoParallel(cl)
 
 setwd("/Users/sherry/Desktop/taiwan_airpollution/airbox")
 
@@ -20,15 +25,32 @@ avg_diff <- function(list){
   return(avg)
 }
 
+unique_length <- function(list){
+  return(length(unique(list)))
+}
+
+#Parallel computing for sptial anomaly detection
+spatial_dectect <- function(a, df, time_panel){
+  temp <- df[df$time_panel == time_panel[a], ]
+  temp_station <- temp[c("lon", "lat")]
+  temp_distance <- distm(temp_station, fun = distHaversine)/1000
+  temp_distance <- temp_distance >0 & temp_distance <= 3
+  neighbor_num <- apply(temp_distance, 2, sum)
+  temp_pm <- temp$PM2.5
+  #operation goes by column
+  temp_neighbor <- apply(temp_pm * temp_distance, 2, sum)/neighbor_num
+  return(test <- temp_pm - temp_neighbor)
+}  
+
 exclude <- c()
 airbox_head <- c("Date", "Time", "device_id", "PM2.5", "PM10", "PM1", "Temperature", "Humidity", "lat", "lon")
 taiwan <- readOGR("/Users/sherry/Desktop/taiwan_airpollution/gadm36_TWN_shp/gadm36_TWN_0.shp")
 airbox_processed <- data.frame(Date <- c(), Time <- c(),
-                         device_id <- c(), PM2.5 <- c(),
-                         PM10 <- c(), PM1 <- c(),
-                         Temperature <- c(), Humidity <- c(),
-                         lat <- c(), lon <- c(),
-                         onland <- c())
+                               device_id <- c(), PM2.5 <- c(),
+                               PM10 <- c(), PM1 <- c(),
+                               Temperature <- c(), Humidity <- c(),
+                               lat <- c(), lon <- c(),
+                               onland <- c())
 
 #loop through the 29,915,720 records using foreach package, orginal loop will be a function
 a = 0
@@ -112,18 +134,27 @@ ggmap(map) +
 
 #sstime <- Sys.time()
 #find the distance for every two stations
-station_distance <- distm(station[c("lon","lat")], fun = distHaversine)/1000
+station_distance <- data.frame(distm(station[c("lon","lat")], fun = distHaversine)/1000)
 #Sys.time() - sstime #Time difference of 7.971666 secs
 p1 <- hist(station_distance[station_distance < 350], breaks = 20)
 p1$counts <- p1$density
 p2 <- density(station_distance[station_distance < 350], bw = 15)
 plot(p1, main = "Station distance density graph", xlab = "Station distance", col = rgb(1, 0, 1, 1/4))
 lines(p2, col = rgb(1, 0, 1, 1))
-
 #use the first peak
 p2 <- cbind(p2$x, p2$y)
 #find the distance that bears the first negative diff
 radius <- round(p2[which(diff(p2[, 2], 1) <= 0)[1], 1])
+#find the neighbor distance
+near_test <- seq(1, 23, by = 0.5)
+distance_test <-sapply(near_test, function(k) {sum(apply(station_distance <= k, 2, sum) <3)/nrow(station_distance)})
+plot(near_test, distance_test, type = "b", pch = 19, frame = FALSE, 
+     main = "Find Neighbor Distance",
+     xlab = "Distance between two points",
+     ylab = "Percentage of No Neighbor")
+
+#neighbor distance is set to 3km
+near <- 3
 
 #temperal data processing
 airbox_processed$unix_time <- paste(airbox_processed$Date, airbox_processed$Time, "GMT")
@@ -137,24 +168,30 @@ airbox_processed <- airbox_processed[-12]
 
 #temporal detection
 airbox_processed <- airbox_processed[order(airbox_processed$device_id, airbox_processed$unix_time), ]
-#keep track of the missing value
 device_id <- unique(airbox_processed$device_id)
-na_stat <- c()
-temporal_anomaly <- c()
+
 #threshold for continue time is 450s.
 interval <- 450
 #determine a good abnormal pm2.5 diff threshold
 a = 1
+pm_check <- data.frame(Var1 <- c(),
+                       var2 <- c(),
+                       Freq <- c())
 for (a in 1:length(device_id)){
   temp <- airbox_processed[airbox_processed$device_id == device_id[a], ]
   temp$time_diff <- diff(c(0, temp$unix_time), 1)
-  temp <- temp[temp$time_diff <= interval]
-  temp$temperal_anomaly[temp$lowtime_diff == FALSE] <- NA
-  temp$temperal_anomaly[temp$lowtime_diff == TRUE & temp$pm25_diff == FALSE] <- TRUE
-  temp$temperal_anomaly[temp$lowtime_diff == TRUE & temp$pm25_diff == TRUE] <- FALSE
-  temporal_anomaly <- c(temporal_anomaly, temp$temperal_anomaly)
-  na_stat <- c(na_stat, round(sum(temp$lowtime_diff)/length(temp$lowtime_diff), 2))
+  temp <- temp[temp$time_diff <= interval, ]
+  temp$pm_diff <- diff(c(0, temp$PM2.5), 1)
+  pm_check <- rbind(pm_check, data.frame(t(table(abs(temp$pm_diff)))))
 }
+pm_check <- pm_check[-1]
+pm_check <- aggregate(pm_check$Freq, by = list(pm_check$Var2), sum)
+pm_check <- pm_check[order(pm_check$x, decreasing = TRUE), ]
+#From accumulated frequencies we can see that pm_threshold is 2.
+
+#keep track of the missing value
+na_stat <- c()
+temporal_anomaly <- c()
 
 a = 1
 for (a in 1:length(device_id)){
@@ -174,8 +211,38 @@ count <- table(airbox_processed$temporal_anomaly, useNA = "always")/length(airbo
 na_stat <- data.frame(device_id, na_stat)
 #barplot(count, main = "Temporal Anomaly", col = rgb(1, 0, 1, 1/4))
 
-temp <- airbox_processed[airbox_processed$temporal_anomaly == TRUE & !is.na(airbox_processed$temporal_anomaly), ]
-temp[3034570:3034585, ]
+#Spatial detection
+airbox_processed <- airbox_processed[order(airbox_processed$unix_time, airbox_processed$device_id), ]
+avg_time_diff <- aggregate(airbox_processed$unix_time, by = list(airbox_processed$device_id), avg_diff)
+#hist(avg_time_diff$x[avg_time_diff$x <=10000], main = "Average Time Interval", xlab = "Less than 10000s interval")
+#Round the unix time to the nearst 450s interval
+time <- airbox_processed$unix_time - airbox_processed$unix_time[1]
+airbox_processed$time_panel <- round(time/450)
+timeslice_length <- aggregate(airbox_processed$device_id, by = list(airbox_processed$time_panel), unique_length)
+#head(timeslice_length[order(timeslice_length$x, decreasing = TRUE),], 10)
+#tail(timeslice_length[order(timeslice_length$x, decreasing = TRUE),], 10)
+time_panel <- unique(airbox_processed$time_panel)
+airbox_processed <- airbox_processed[order(airbox_processed$time_panel), ]
+#stime <- Sys.time()
+spatial_anomaly <- foreach(a = 1:length(time_panel), .combine = c, .packages = "geosphere") %dopar% spatial_dectect(a, airbox_processed, time_panel)
+#Sys.time() - stime
+#summary(spatial_anomaly)
+hist(abs(spatial_anomaly)[which(abs(spatial_anomaly) < 10)], main = "Histgram of abs less than 10", xlab = "abs")
+#spatial anomaly threshold is abs more than 4
+spatial_relative_degree <- spatial_anomaly
+spatial_relative_degree[spatial_relative_degree > 4] <- "High"
+spatial_relative_degree[spatial_relative_degree <  -4] <- "Low"
+spatial_relative_degree[spatial_relative_degree >= -4 & spatial_relative_degree <= 4] <- "Normal"
+#table(spatial_relative_degree)
+spatial_anomaly <- abs(spatial_anomaly)
+spatial_anomaly[spatial_anomaly > 4] <- "TRUE"
+spatial_anomaly[spatial_anomaly <= 4] <- "FALSE"
+spatial_anomaly <- as.logical(spatial_anomaly)
+#table(spatial_anomaly, useNA = "always")
+airbox_processed$spatial_relative_degree <- spatial_relative_degree
+airbox_processed$spatial_anomaly <- spatial_anomaly
+airbox_processed$sp_tem_anomaly <- airbox_processed$temporal_anomaly & airbox_processed$spatial_anomaly
+#table(airbox_processed$sp_tem_anomaly, useNA = "always")/nrow(airbox_processed)
+#summary(airbox_processed)
 
-
-
+write.csv(airbox_processed, "airbox_processed.csv")
